db6178773f7a3269d323fe6fe991e7d7af83b6bc
danielprevidi <daniel-previdi@hotmail.comgit config --global user.email daniel-previdi@hotmail.com>
2026-03-26 20:41:04 -0300

    exercicio 5

---
+++ 20260326/5.menuBebidas.js
@@ -0,0 +1,16 @@
+let bebidas = 3;
+
+switch(bebidas) {
+    case 1:
+        console.log("Água");
+        break;
+    case 2:
+        console.log("Refrigerante");
+        break;
+    case 3:
+        console.log("Cerveja");
+        break;
+    default:
+        console.log("Não encontrado");
+        break;
+}
