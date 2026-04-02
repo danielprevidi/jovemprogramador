ff83f14cfdf728b49531695fd5e2c8b1cdd4baf1
danielprevidi <daniel-previdi@hotmail.comgit config --global user.email daniel-previdi@hotmail.com>
2026-04-01 21:54:05 -0300

    exercicios

---
+++ 20260331_estudos/array.js
@@ -0,0 +1,11 @@
+const frutas = [];
+
+const fruta1 = prompt("Digite a primeira fruta:");
+const fruta2 = prompt("Digite a segunda fruta:");
+const fruta3 = prompt("Digite a terceira fruta:");
+
+frutas.push(fruta1);
+frutas.push(fruta2);
+frutas.push(fruta3);
+
+console.log(frutas);
---
+++ 20260401/tabuadaDoCincoWhille.js
@@ -0,0 +1,8 @@
+
+let i = 1
+let tabuada = 10
+
+while (i <= 10) {
+  console.log(tabuada + " x " + i + " = " +  tabuada * i);
+  i++;
+}   
