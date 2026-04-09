ab07c7d8f0d52f0d160872e3eba3d0e1fc051f38
danielprevidi <daniel-previdi@hotmail.comgit config --global user.email daniel-previdi@hotmail.com>
2026-04-08 21:51:37 -0300

    exercicios calculadora

---
+++ 20260402/controleEstufa.js
@@ -0,0 +1,15 @@
+// CONTROLE DE TEMPERATURA DE ESTUFA
+
+let temperaturaInicial = 26;
+let aumentoTemperatura = 10;
+let abaixaTemperatura = 5;
+let novaTemperaturaAumento = temperaturaInicial + trocaTemperatura
+let novaTemperaturaAbaixo = 
+
+
+// Opção 1: Aumentar temperatura
+if ((temperaturaInicial + trocaTemperatura) > temperaturaInicial) {
+console.log("Temperatura aumentada para: " + novaTemperatura + "°C");
+}
+
+
